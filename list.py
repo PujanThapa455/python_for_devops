24b708e544a4966c480ed92dd431e9a9a73a405f
PujanThapa <poojanthapa45@gmail.com>
2025-07-05 21:43:55 +0545

    list

---
+++ list.py
@@ -0,0 +1,7 @@
+options=["male","female","other","none"]
+check = input("Enter the value to check:")
+if check in options:
+    print("Yes,its present")
+else:
+    print("No,its not present")
+    
